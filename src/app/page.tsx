838f1c7c429f1b80d46d14bf88c29705bb48e4dd
mpotane <65337802+mpotane@users.noreply.github.com>
2023-08-13 19:14:47 +0800

    add scroll-area

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,44 +4,44 @@ import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/mode-toggle";
 import { useChat } from "ai/react";
 import { Input } from "@/components/ui/input";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function Home() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
 
   return (
     <main className="container">
-      <header className="w-full">
-        <nav className="flex items-center justify-between p-5">
-          <div>LOGO</div>
-          <div className="flex gap-5">
-            <a href="#">LINK 1</a>
-            <a href="#">LINK 2</a>
-            <a href="#">LINK 3</a>
-            <a href="#">LINK 4</a>
-          </div>
+      <header className="mx-auto max-w-lg">
+        <nav className="flex items-center justify-between py-5">
+          <div>Markybot</div>
           <div>
             <ModeToggle />
           </div>
         </nav>
       </header>
-      <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
-        {messages.map((m) => (
-          <div key={m.id}>
-            {m.role === "user" ? "User: " : "AI: "}
+      <ScrollArea className="mx-auto w-full max-w-md p-5 flex flex-col h-[48rem] bg-slate-700 rounded">
+        {messages.map((m) => {
+          const colorClassName = m.role === "user" ? "bg-sky-600" : "bg-slate-50 text-black";
+          return (
+          <div key={m.id} className={`${colorClassName} rounded-md mb-5 p-2 `}>
+            {m.role === "user" ? "🧑 " : "🤖 "}
             {m.content}
           </div>
-        ))}
+          )
+        })}
 
         <form onSubmit={handleSubmit}>
-          <div className="max-w-md w-full fixed flex gap-2 bottom-0 mb-8">
+          <div className="max-w-md fixed flex items-center justify-center gap-2 bottom-0 mb-8">
             <Input
+              type="text"
+              className="w-[20rem]"
               value={input}
               onChange={handleInputChange}
             />
             <Button type="submit">Send</Button>
           </div>
         </form>
-      </div>
+      </ScrollArea>
     </main>
   );
 }
