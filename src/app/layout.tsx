de5877687b2b4143023c9b2e9f4b6f9db90c792e
mpotane <65337802+mpotane@users.noreply.github.com>
2023-09-06 13:48:18 +0800

    remove ss auth for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
-import { getServerSession } from "next-auth";
 import SessionProvider from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,17 +12,16 @@ export const metadata: Metadata = {
     "Markybot is an AI-powered personal chatbot created by Mpotane using langchain and openai. Get instant responses to your queries and have a personalized conversation with Markybot today!",
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession();
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <SessionProvider session={session}>{children}</SessionProvider>
+          <SessionProvider>{children}</SessionProvider>
         </ThemeProvider>
       </body>
     </html>
