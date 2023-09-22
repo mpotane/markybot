890e68695e0fb17e2c02ce303a74d556ef1e2e64
mpotane <65337802+mpotane@users.noreply.github.com>
2023-09-22 12:32:04 +0800

    Revert "use server session"
    This reverts commit dbf62fc490e3d50a6ac5e72722dc4fc06a4084aa.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import { SessionProvider } from "./provider";
-import { getServerSession } from "next-auth";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,17 +12,16 @@ export const metadata: Metadata = {
     "Markybot is an AI-powered personal chatbot created by Mpotane using langchain and openai. Get instant responses to your queries and have a personalized conversation with Markybot today!",
 };
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession();
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <SessionProvider session={session}>{children}</SessionProvider>
+          <SessionProvider>{children}</SessionProvider>
         </ThemeProvider>
       </body>
     </html>
